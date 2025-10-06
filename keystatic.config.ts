59eb36f7346205fed09d53b4583cc217a2972f08
Pancham <75246178+Pancham555@users.noreply.github.com>
2025-10-06 11:57:28 +0530

    Tried adding keystatic cms

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -1,9 +1,12 @@
 import { articlesKs, authorsKs, categoriesKs } from "@/lib/keystatic";
 import { config } from "@keystatic/core";
+// import { loadEnv } from "vite";
 
+// const { REPO_OWNER,REPO_NAME } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 export default config({
   storage: {
-    kind: "local",
+    kind: "github",
+    repo: `Pancham555/astro-news`
   },
   ui: {
     brand: {
